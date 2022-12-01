917a73ebb6c8861ec0690f0d1de9fe260aeb82e5
MichaelTwG <michaelgolenia1@gmail.com>
2022-12-01 19:44:47 -0300

    Task 6: Render templete - if, else

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -41,11 +41,12 @@ def number_template(n):
 
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
-    if (n % 2) == 1:
-        text = "Number: {} is odd".format(n)
+    if n % 2 == 0:
+        text = '{} is even'.format(n)
     else:
-        text = "Number: {} is even".format(n)
-    return render_template("6-number_odd_or_even.html", text=text)
+        text = '{} is odd'.format(n)
+    return render_template('6-number_odd_or_even.html', text=text)
+
 
 if __name__ == "__main__":
     app.run(port=5000, host="0.0.0.0", debug=True)
