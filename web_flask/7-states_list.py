d0858d1eef395eec02c32f6fe2a96ecd8c175154
MichaelTwG <michaelgolenia1@gmail.com>
2022-12-02 08:36:33 -0300

    WIP: Task 7 - List in a Li all States - With DB or FileStorage

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Module 7-states_list """
+
+from flask import Flask, render_template
+from markupsafe import escape
+from models import storage
+
+
+app = Flask(__name__)
+
+# SET STRICT SLASHES FOT ALL ROUTES
+app.url_map.strict_slashes = False
+# Add app.teardown_appcontxt - call storage.clos after each request
+@app.teardown_appcontext
+def teardown(exit):
+    storage.close()
+
+app.route("/states_list")
+def show_states_list():    
+    return render_template("7-states_list.html", states=storage.all("State"))
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0", debug=True)
